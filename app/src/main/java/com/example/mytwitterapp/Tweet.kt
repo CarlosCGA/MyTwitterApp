310560ff83edcc2a0aa1c9923c7531e095dd83cc
Carlos <ccaieg00@estudiantes.unileon.es>
2024-04-07 22:26:12 +0200

    upgrade tweet composable

--- app/src/main/java/com/example/mytwitterapp/Tweet.kt
+++ app/src/main/java/com/example/mytwitterapp/Tweet.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -25,6 +26,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -204,9 +206,13 @@ fun ContentText(text: String) {
 @Composable
 fun ContentImage() {
     Image(
-        modifier = Modifier.clip(RoundedCornerShape(24.dp)),
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(24.dp))
+            .height(200.dp),
         painter = painterResource(id = R.drawable.profile),
-        contentDescription = "Image"
+        contentDescription = "Image",
+        contentScale = ContentScale.Crop
     )
 }
 
